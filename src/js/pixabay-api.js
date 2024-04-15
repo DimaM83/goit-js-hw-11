57ec57c2f17aad2b7ba5082ca8bcf37724f92f74
DimaM83 <mdv16.01.83@gmail.com>
2024-04-16 00:10:20 +0200

    hw-11

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,23 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+export async function doFetch(aim, textToWait) {
+  const API_KEY = '16991331-df0a6792d36af314f174a3b15';
+  const url = 'https://pixabay.com/api/';
+
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: aim,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 9,
+  });
+
+  return fetch(`${url}?${params}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
